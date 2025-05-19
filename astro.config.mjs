11d8e1f7a45238aca43a2a4cff2f3fc4dfc7cde0
Marta González <hola@martagonzalez.dev>
2025-05-19 16:24:41 +0200

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://martacg.github.io/martagonzalez-designsystem',
+	site: 'https://martacg.github.io',
   	base: 'martagonzalez-designsystem',
 	integrations: [
 		starlight({
